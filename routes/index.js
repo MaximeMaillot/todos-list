cc9ee15391d4d42d4647e166e58f0014e3dddf24
Martial Maillot <martial.maillot@gmail.com>
2020-04-04 17:00:13 +0200

    Use ES6 import in palce of require.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
+
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('Hello World');
 });
 
-module.exports = router;
+export { router };
