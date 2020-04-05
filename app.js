aafd5126337b9072b8a5b6bfa906092481454961
MaximeMaillot <maillot.maxime62@gmail.com>
2020-04-05 12:28:52 +0200

    create table todo at the start of the server

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 import express from 'express';
 import logger from 'morgan';
-import './env.js'
+import './env.js';
+import { db } from './db/dbConnection.js'
 import { router as indexRouter } from './routes/index.js';
 import { router as todoRouter } from './routes/todos.js';
+
+db.none(
+    'CREATE TABLE IF NOT EXISTS todo('
+    + 'id serial NOT NULL PRIMARY KEY,'
+    + 'name text NOT NULL,'
+    + 'date date NOT NULL DEFAULT CURRENT_DATE)')
+    .then(function (data) {
+      console.log('table was created if it did not exist');
+    })
+    .catch(function (error) {
+      throw error;
+    })
 
 var app = express();
 
---
+++ db/dbConnection.js
@@ -0,0 +1,12 @@
+import pgp from "pg-promise";
+
+const dbConnectionData = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+};
+const db = pgp()(dbConnectionData);
+
+export { db };
