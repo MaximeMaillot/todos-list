91e18a3ca2b612b29dced0302c60cfc15fcfee2a
MaximeMaillot <maillot.maxime62@gmail.com>
2020-04-05 12:28:52 +0200

    create table todo at the start of the server

--- routes/todos.js
+++ routes/todos.js
@@ -1,15 +1,17 @@
 import express from 'express';
 const router = express.Router();
-import pgp from "pg-promise";
+import { db } from '../db/dbConnection.js';
 
-const dbConnectionData = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-};
-const db = pgp()(dbConnectionData);
+// import pgp from "pg-promise";
+
+// const dbConnectionData = {
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_DATABASE,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+// };
+// const db = pgp()(dbConnectionData);
 
 /**
  * get a list of all the todos
