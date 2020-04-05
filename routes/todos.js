aab593e398ec15d2e86de1e21dfd94f1e476ed2f
MaximeMaillot <maillot.maxime62@gmail.com>
2020-04-05 12:34:08 +0200

    clean code

--- routes/todos.js
+++ routes/todos.js
@@ -2,17 +2,6 @@ import express from 'express';
 const router = express.Router();
 import { db } from '../db/dbConnection.js';
 
-// import pgp from "pg-promise";
-
-// const dbConnectionData = {
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   database: process.env.DB_DATABASE,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-// };
-// const db = pgp()(dbConnectionData);
-
 /**
  * get a list of all the todos
  */
