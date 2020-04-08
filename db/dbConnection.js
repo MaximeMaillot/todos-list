48e9ae5931a0e999e51d4cf52f225a62ebb98024
MaximeMaillot <maillot.maxime62@gmail.com>
2020-04-08 17:11:10 +0200

    using a different .env for the test environment

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -2,30 +2,13 @@
 import pgp from "pg-promise";
 
 let db;
-let environment = false;
 
-process.argv.forEach((element) => {
-  if (element.includes("mocha")) {
-    environment = "test";
-  }
+db = pgp()({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
-
-if (environment === "test") {
-  db = pgp()({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE_TEST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-  });
-} else {
-  db = pgp()({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-  });
-}
 
 export { db };
